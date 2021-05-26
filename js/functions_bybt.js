610b82e12d6b9a70565320d9e59d17a11cb9eb22
digitalguy99 <brtsuper07@gmail.com>
2021-05-26 19:21:07 +0900

    changed visitor counter to count unique visitors instead of page views

--- js/functions_bybt.js
+++ js/functions_bybt.js
@@ -27,11 +27,17 @@ function demo() {
     document.getElementById("gsc-i-id1").setAttribute("placeholder", "Penelusuran");
 }
 
-//visitor counter
+//visitor counter(old)
 
+// function siteVisitor() {
+//   var counterData = document.getElementById("visitorCounter");
+//   counterData.innerHTML = '<script type="text/javascript" src="https://www.freevisitorcounters.com/en/home/counter/744451/t/5"><\/script>' + '<p>Anda pengunjung ke-</p>' + '<a href="https://www.freevisitorcounters.com/en/home/stats/id/744451" target="_blank"><img src="https://www.freevisitorcounters.com/en/counter/render/744451/t/5" border="0" class="counterimg"></a>';
+// }
+
+//visitor counter(new)
 function siteVisitor() {
   var counterData = document.getElementById("visitorCounter");
-  counterData.innerHTML = '<script type="text/javascript" src="https://www.freevisitorcounters.com/en/home/counter/744451/t/5"><\/script>' + '<p>Anda pengunjung ke-</p>' + '<a href="https://www.freevisitorcounters.com/en/home/stats/id/744451" target="_blank"><img src="https://www.freevisitorcounters.com/en/counter/render/744451/t/5" border="0" class="counterimg"></a>';
+  counterData.innerHTML = '<p>Anda pengunjung ke-</p>'+'<a href="https://www.hitwebcounter.com" target="_blank">' + '<img src="https://hitwebcounter.com/counter/counter.php?page=7817684&style=0006&nbdigits=5&type=ip&initCount=0" title="Free Counter" Alt="web counter" border="0" /></a>';
 }
 
 //twitter feed expansion
